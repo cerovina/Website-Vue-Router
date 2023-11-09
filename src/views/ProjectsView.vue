cb9dbd8861dd57397380181b12f50d0c94021ceb
unknown <cerovina1997@gmail.com>
2023-11-09 16:57:00 +0100

    Minor changes

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -17,17 +17,17 @@
           <div class="grid-item">
             <img class="img1" src="@/assets/formpicture.png" alt="Web Form">
             <br>
-            <a class="link" href="https://web-form-vue-lake.vercel.app/">Form Validation app (Vue.js)</a>
+            <a class="link" href="https://web-form-vue-lake.vercel.app/">Form Validation (Vue.js)</a>
           </div>
           <div class="grid-item">
             <img class="img1" src="@/assets/yetiii.png" alt="Weather Yeti">
             <br>
-            <a class="link" href="https://cerovina.github.io/Weather-Yeti/">Weather Yeti app (React.js)</a>
+            <a class="link" href="https://cerovina.github.io/Weather-Yeti/">Weather Yeti (React.js)</a>
           </div>
           <div class="grid-item">
             <img class="img1" src="@/assets/bugs.png" alt="Space Shooter">
             <br>
-            <a class="link" href="https://cerovina.github.io/Grim-Shooter/">Grim Shooter app (p5.js)</a>
+            <a class="link" href="https://cerovina.github.io/Grim-Shooter/">Grim Shooter (p5.js)</a>
           </div>
         </div>
         
