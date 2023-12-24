d6a5e9467f2e573483c1040d0dab3e5f3ab6ecdf
unknown <cerovina1997@gmail.com>
2023-12-24 17:15:29 +0100

    Minor style changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,7 +12,7 @@
         <span class="yellow">></span> Proficient in HTML, CSS, and JavaScript. Experienced in Vue and React.
         <br>
         <br>
-        <span class="yellow">></span> Familiar with multiple other technologies such as Node, C# and p5.
+        <span class="yellow">></span> Familiar with multiple other technologies such as Node, p5 and C#.
         <br>
         <br>
         <span class="yellow">></span> Passionate about web development and expanding my skills in the field.
